f0a1104ecf4a7e1459614b29cafe7d18c550182f
InHyeok Jang <156999923+JangIn-40@users.noreply.github.com>
2024-11-16 23:49:32 +0900

    Time: 0 ms (100%), Space: 9.1 MB (12.09%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        	int n = matrix.size() / 2;
-	int start{}, end = matrix[0].size() - 1;
+        int n = matrix.size() / 2;
+        int start{}, end = matrix[0].size() - 1;
 
-	while(start < end)
-	{
-		std::vector<int> temp;
-		for (int i = start; i <= end; ++i)
-		{
-			temp.push_back(matrix[start][i]);
-		}
+        while(start < end)
+        {
+            std::vector<int> temp;
+            for (int i = start; i <= end; ++i)
+            {
+                temp.push_back(matrix[start][i]);
+            }
 
-		for (int i = start, j = 0; i < end; ++i, ++j)
-		{
-			matrix[start][end - j] = matrix[i][start];
-			matrix[i][start] = matrix[end][i];
-			matrix[end][i] = matrix[end - j][end];
-			matrix[end - j][end] = temp[end - i];
-		}
+            for (int i = start, j = 0; i < end; ++i, ++j)
+            {
+                matrix[start][end - j] = matrix[i][start];
+                matrix[i][start] = matrix[end][i];
+                matrix[end][i] = matrix[end - j][end];
+                matrix[end - j][end] = temp[end - i];
+            }
 
-		++start;
-		--end;
-	}
+            ++start;
+            --end;
+        }
     }
 };
