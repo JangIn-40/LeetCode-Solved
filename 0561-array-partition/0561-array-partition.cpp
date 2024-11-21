0a29cdb403d12fcb1e5d4e3f0b7e01452d91d3d8
InHyeok Jang <156999923+JangIn-40@users.noreply.github.com>
2024-11-22 00:04:41 +0900

    Time: 11 ms (60.14%), Space: 39.1 MB (23.35%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        int big = *max_element(nums.begin(), nums.end());
-        int small = *min_element(nums.begin(), nums.end());
-
-        vector<int> result = CountingSort(nums, big, small);
+        vector<int> result = RadixSort(nums);
 
         int sum{};
         for(int i = 0; i < result.size(); i += 2)
@@ -16,54 +13,69 @@ public:
     }
 
 private:
-    vector<int> CountingSort(const vector<int>& nums, int big, int small)
+    void CountingSort(vector<int>& nums, int exp)
     {
-        vector<int> countArray;
-        if(small < 0)
+        if(nums.empty())
         {
-            countArray.resize(big - small + 1);
-
-            for(const auto& num : nums)
-            {
-                ++countArray[num - small];
-            }
-            for(int i = 1; i < countArray.size(); ++i)
-            {
-                countArray[i] += countArray[i - 1];
-            }
-
-            vector<int> output(nums.size(), 0);
-            for(int i = nums.size() - 1; i >= 0; --i)
-            {
-                int value = nums[i] - small;
-                int index = --countArray[value];
-
-                output[index] = value + small;
-            }
-            return output;
+            return;
         }
-        else
+
+        vector<int> count(10, 0);
+
+        for(const auto& num : nums)
         {
-            countArray.resize(big + 1);
-
-            for(const auto& num : nums)
-            {
-                ++countArray[num];
-            }
-            for(int i = 1; i < countArray.size(); ++i)
-            {
-                countArray[i] += countArray[i - 1];
-            }
-
-            vector<int> output(nums.size(), 0);
-            for(int i = nums.size() - 1; i >= 0; --i)
-            {
-                int value = nums[i];
-                int index = --countArray[value];
-
-                output[index] = value;
-            }
-            return output;
+            int index = abs(num) / exp % 10;
+            ++count[index];
         }
+        for(int i = 1; i < count.size(); ++i)
+        {
+            count[i] += count[i - 1];
+        }
+
+        vector<int> output(nums.size());
+        for(int i = nums.size() - 1; i >= 0; --i)
+        {
+            int value = abs(nums[i]) / exp % 10;
+            int index = --count[value];
+
+            output[index] = nums[i];
+        }
+
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            nums[i] = output[i];
+        }
+    }
+
+    vector<int> RadixSort(const vector<int>& nums)
+    {
+        vector<int> negatives, positives;
+
+        for(const auto& num : nums)
+        {
+            if(num < 0)
+            {
+                negatives.push_back(num);
+            }
+            else
+            {
+                positives.push_back(num);
+            }
+        }
+
+        int maxNegative = negatives.empty() ? 0 : abs(*min_element(negatives.begin(), negatives.end()));
+        int maxPositive = positives.empty() ? 0 : abs(*max_element(positives.begin(), positives.end()));
+        int maxValue = max(maxNegative, maxPositive);
+
+        for(int exp = 1; maxValue / exp > 0; exp *= 10)
+        {
+            CountingSort(negatives, exp);
+            CountingSort(positives, exp);
+        }
+
+        reverse(negatives.begin(), negatives.end());
+        negatives.insert(negatives.end(), positives.begin(), positives.end());
+
+        return negatives;
     }
 };
