5df3494f7abb6fa19162706213fd9dfbe1c64c19
InHyeok Jang <156999923+JangIn-40@users.noreply.github.com>
2024-12-06 21:15:41 +0900

    Time: 1 ms (18.02%), Space: 9.7 MB (9.44%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> answer;
-        vector<int> row;
-        answer.push_back(vector<int>(1, 1));
+        vector<vector<int>> pascalTriangle;
 
         for(int i = 0; i < numRows; ++i)
         {
-            for(int j = 0; i - 1 >= 0 && j < answer[i - 1].size() + 1; ++j)
+            pascalTriangle.push_back(vector<int>(i + 1, 1));
+            for(int j = 1; j < pascalTriangle[i].size() - 1; ++j)
             {
-                int sum{};
-                if(j - 1 >= 0)
-                {
-                    sum += answer[i - 1][j - 1];
-                }
-                if(j < answer[i - 1].size())
-                {
-                    sum += answer[i - 1][j];
-                }
-
-                row.push_back(sum);
+                pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
             }
-
-            if(i != 0)
-            {
-                answer.push_back(row);
-                row.clear();
-            }
-
         }
 
-        return answer;
+        return pascalTriangle;
     }
 };
