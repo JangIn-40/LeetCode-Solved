32a1ecd6924549ce925923840b823b70cb116596
InHyeok Jang <156999923+JangIn-40@users.noreply.github.com>
2025-03-28 21:51:21 +0900

    Time: 0 ms (100%), Space: 11.1 MB (48.79%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int sum{}, count{};
+        set<int> s;
 
         for(int i = 0; i < nums.size(); ++i)
         {
-            if(nums[i] == 0 || i - 1 >= 0 && nums[i] == nums[i - 1])
+            if(nums[i] != 0)
             {
-                continue; 
-            }
-
-            sum += nums[i] - sum;
-            ++count;
-
-            if(sum == nums.back())
-            {
-                break;
+                s.emplace(nums[i]);
             }
         }
 
-        return count;
+        return s.size();
     }
 };
